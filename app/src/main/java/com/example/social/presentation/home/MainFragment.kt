c98195e007a4cd522b691b757aab0018bd1f952a
Sushil <kmrsushil10@gmail.com>
2023-01-18 12:55:42 +0530

    spannable text view added

--- app/src/main/java/com/example/social/presentation/home/MainFragment.kt
+++ app/src/main/java/com/example/social/presentation/home/MainFragment.kt
@@ -1,12 +1,17 @@
 package com.example.social.presentation.home
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.social.R
 import com.example.social.databinding.FragmentMainBinding
 
 class MainFragment:Fragment() {
@@ -36,6 +41,19 @@ class MainFragment:Fragment() {
         binding.quickTipRecyclerView.layoutManager = LinearLayoutManager(
             requireContext(),LinearLayoutManager.HORIZONTAL,false)
         binding.quickTipRecyclerView.adapter = quickTipsAdapter
+
+
+        val connectionText = SpannableString("Whats happening in \n your circle ? ")
+        connectionText.setSpan(ForegroundColorSpan(resources.getColor(R.color.blue_text)), 0, 5, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.connectionTextView.textView.text = connectionText
+
+
+        val offerText = SpannableString("New Offers")
+        offerText.setSpan(ForegroundColorSpan(resources.getColor(R.color.blue_text)), 0, 3, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.offerText.textView.text = offerText
+
+
+
 
         observeFromViewModal()
         viewModel.getStatus(requireContext())
