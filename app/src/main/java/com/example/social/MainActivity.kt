b0363bc07215c8a8abcd0c4068886b2831e3acd4
Sushil <kmrsushil10@gmail.com>
2023-01-21 14:16:11 +0530

    bottom nav bar setup

--- app/src/main/java/com/example/social/MainActivity.kt
+++ app/src/main/java/com/example/social/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.social
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.social.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +15,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        val navController = findNavController() ?: return
+//
+//        navController.addOnDestinationChangedListener { _, destination, _ ->
+//
+//            Log.d("MainActivity", "addOnDestinationChangedListener: ${destination.id}")
+//            when (destination.id) {
+//                R.id.nav_main -> binding.bottomNavigationView.visibility = View.VISIBLE
+//                R.id.nav_feed -> binding.bottomNavigationView.visibility = View.VISIBLE
+//                R.id.nav_shop -> binding.bottomNavigationView.visibility = View.VISIBLE
+//                R.id.nav_account -> binding.bottomNavigationView.visibility = View.VISIBLE
+//                else -> binding.bottomNavigationView.visibility = View.GONE
+//            }
+//        }
+//
+//        binding.bottomNavigationView.setupWithNavController(navController)
+
     }
 }
