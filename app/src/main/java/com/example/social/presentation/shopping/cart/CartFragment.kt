8cc022cf65b30548a143f16667a8ecf1350eccd1
Sushil <kmrsushil10@gmail.com>
2023-01-26 21:18:35 +0530

    refined layout

--- app/src/main/java/com/example/social/presentation/shopping/cart/CartFragment.kt
+++ app/src/main/java/com/example/social/presentation/shopping/cart/CartFragment.kt
@@ -6,24 +6,28 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.social.databinding.FragmentCartBinding
+import com.example.social.databinding.FragmentEmptyCartBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class CartFragment() : Fragment() {
     private lateinit var binding: FragmentCartBinding
+    private lateinit var emptyCartBinding: FragmentEmptyCartBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentCartBinding.inflate(layoutInflater , container,false)
-        return binding.root
+//        binding = FragmentCartBinding.inflate(layoutInflater , container,false)
+//        return binding.root
+        emptyCartBinding = FragmentEmptyCartBinding.inflate(layoutInflater,container,false)
+        return emptyCartBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.checkoutButton.setOnClickListener {
-            val bottomSheetFragment :BottomSheetDialogFragment = OrderPlaceBottomSheetFragment()
-            bottomSheetFragment.show(requireFragmentManager(), bottomSheetFragment.tag)
-        }
+//        binding.checkoutButton.setOnClickListener {
+//            val bottomSheetFragment :BottomSheetDialogFragment = OrderPlaceBottomSheetFragment()
+//            bottomSheetFragment.show(requireFragmentManager(), bottomSheetFragment.tag)
+//        }
     }
 }
